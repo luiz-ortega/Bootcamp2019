40403ef7d877970edfd19b196f54a011b7b2f1fc
luby <luiz@luby.com.br>
2019-06-26 20:54:39 -0300

    Desafio01 - Correções

--- desafio01/index.js
+++ desafio01/index.js
@@ -1,46 +1,100 @@
 const express = require("express");
+
 const server = express();
 
 server.use(express.json());
 
-const projetos = [];
+/**
+ * Utilizamos a variável `numberOfRequests` como
+ * `let` porque vai sofrer mutação. A variável
+ * `projects` pode ser `const` porque um `array`
+ * pode receber adições ou exclusões mesmo sendo
+ * uma constante.
+ */
+let numberOfRequests = 0;
+const projects = [];
 
+/**
+ * Middleware que checa se o projeto existe
+ */
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id);
+
+  if (!project) {
+    return res.status(400).json({ error: "Project not found" });
+  }
+
+  return next();
+}
+
+/**
+ * Middleware que dá log no número de requisições
+ */
+function logRequests(req, res, next) {
+  numberOfRequests++;
+
+  console.log(`Número de requisições: ${numberOfRequests}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+/**
+ * Projects
+ */
 server.get("/projects", (req, res) => {
-  return res.json(projetos);
+  return res.json(projects);
 });
 
 server.post("/projects", (req, res) => {
-  const { projeto } = req.body;
-  projeto["tasks"] = [];
-  projetos.push(projeto);
+  const { id, title } = req.body;
 
-  return res.json(projetos);
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
 });
 
-server.post("/projects/:index/tasks", (req, res) => {
+server.put("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
   const { title } = req.body;
-  const { index } = req.params;
 
-  projetos[index - 1]["tasks"].push(title);
+  const project = projects.find(p => p.id === id);
 
-  return res.json(projetos);
+  project.title = title;
+
+  return res.json(project);
 });
 
-server.put("/projects/:index", (req, res) => {
+server.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+/**
+ * Tasks
+ */
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+  const { id } = req.params;
   const { title } = req.body;
-  const { index } = req.params;
 
-  projetos[index - 1]["title"] = title;
+  const project = projects.find(p => p.id === id);
 
-  return res.json(projetos);
-});
+  project.tasks.push(title);
 
-server.delete("/projects/:index", (req, res) => {
-  const { index } = req.params;
-
-  projetos.splice(index - 1, 1);
-
-  return res.json(projetos);
+  return res.json(project);
 });
 
 server.listen(3000);
