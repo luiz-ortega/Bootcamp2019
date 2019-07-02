0d8d72643cfb8d8f4496a43aba07e6185af9c4bf
luby <luiz@luby.com.br>
2019-07-02 09:48:39 -0300

    Upload user avatar fix

--- modulo03/src/config/database.js
+++ modulo03/src/config/database.js
@@ -2,18 +2,6 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'docker',
-    database: 'gobarber',
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-    },
-};
-module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
     password: 'password',
     database: 'gobarber',
     define: {
