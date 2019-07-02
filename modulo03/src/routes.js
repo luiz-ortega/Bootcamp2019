378d9e59f7a720dca7a04cb1bc92f760d063477b
luby <luiz@luby.com.br>
2019-07-02 19:46:35 -0300

    List appointments

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -21,7 +21,8 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/appointment', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
