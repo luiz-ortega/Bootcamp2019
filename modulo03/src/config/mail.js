01ea18e0b4508619c47e5e0853fee72e5c662811
luiz-ortega <luizngust@hotmail.com>
2019-07-03 12:23:10 -0300

    Send email notification for deleted appointments

---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'c0abf0af5d3bb6',
+        pass: '7cd2f336f3960d',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+    },
+};
