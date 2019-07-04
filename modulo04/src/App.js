a23a208378dd9bb2c504706847ccf8725450b71c
luby <luiz@luby.com.br>
2019-07-04 19:02:38 -0300

    image-loader

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import profile from "./assets/Selection_005.png";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <img src={profile} />;
 }
 
 export default App;
