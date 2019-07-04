1a08e07e52144191c2c6f57bb316865d7e73e39e
luby <luiz@luby.com.br>
2019-07-04 19:45:49 -0300

    crud

---
+++ modulo04/src/components/TechList.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: ["Node.js", "React.js", "React Native "]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ techs: [...this.state.techs, this.state.newTech] });
+    this.state.newTech = "";
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Adicionar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
