5f2824dbb737f4645d0c322d3e12212a9d7cc1fa
Luiz Ortega <luizngust@hotmail.com>
2019-07-07 18:36:41 -0300

    main page styles

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Title } from './styles';
-
-// import { Container } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-    return <Title error={false}>Hello World</Title>;
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositórios
+            </h1>
+
+            <Form onSubmit={() => {}}>
+                <input type="text" placeholder="Adicionar repositório" />
+
+                <SubmitButton disabled>
+                    <FaPlus color="#fff" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
