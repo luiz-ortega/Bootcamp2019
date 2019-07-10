7e1dfd8c2fe6b7ef2e43649c8e3fa8e623a4f4be
Luiz Ortega <luizngust@hotmail.com>
2019-07-09 21:15:25 -0300

    styling span

--- modulo05/src/pages/Repository/styles.js
+++ modulo05/src/pages/Repository/styles.js
@@ -80,6 +80,17 @@ export const IssuesList = styled.ul`
                         color: #7159c1;
                     }
                 }
+
+                span {
+                    background: #eee;
+                    color: #333;
+                    border-radius: 2px;
+                    font-size: 12px;
+                    font-weight: 600;
+                    height: 20px;
+                    padding: 3px 4px;
+                    margin: 10px;
+                }
             }
         }
 
