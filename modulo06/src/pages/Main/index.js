8b462e69c99772f1d48ace2465d1483079223cd8
luby <luizngust@hotmail.com>
2019-07-22 08:56:30 -0300

    merge

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from 'react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {
