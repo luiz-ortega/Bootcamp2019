a2e413e1437eca31fdb43e0a3342a906977cbb7a
luby <luizngust@hotmail.com>
2019-07-24 16:49:03 -0300

    Signup new users

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 const schema = Yup.object().shape({
     name: Yup.string().required('O nome é obrigatório'),
     email: Yup.string()
-        .email('Insira um em-ail válido')
+        .email('Insira um email válido')
         .required('O e-mail é obrigatório'),
     password: Yup.string()
         .min(6, 'A senha precida de pelo menos 6 caracteres.')
@@ -16,8 +19,10 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
-    function handleSubmit(data) {
-        console.tron.log(data);
+    const dispatch = useDispatch();
+
+    function handleSubmit({ name, email, password }) {
+        dispatch(signUpRequest(name, email, password));
     }
 
     return (
