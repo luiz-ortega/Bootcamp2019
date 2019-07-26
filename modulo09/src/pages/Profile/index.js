90a713c6cc4df44a78729e23a921d26bae0addf5
Luiz Ortega <luizngust@hotmail.com>
2019-07-26 00:43:08 -0300

    profile page styles and basic user info loading

--- modulo09/src/pages/Profile/index.js
+++ modulo09/src/pages/Profile/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-    return <div />;
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {}
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome completo" />
+                <Input name="email" placeholder="Seu endereço" />
+                <hr />
+
+                <Input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Sua senha antiga"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Sua nova senha"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confrime sua senha nova"
+                />
+
+                <button type="button">Atualizar perfil</button>
+            </Form>
+
+            <button type="button">Sair do GoBarber</button>
+        </Container>
+    );
 }
