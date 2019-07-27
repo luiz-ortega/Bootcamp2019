e277bea343aaa171150ea1cbf97cee19c8c34a98
Luiz Ortega <luizngust@hotmail.com>
2019-07-27 13:32:52 -0300

    first

---
+++ modulo10/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+    return (
+        <SafeAreaView>
+            <Text>Hello Wolrd</Text>
+        </SafeAreaView>
+    );
+}
