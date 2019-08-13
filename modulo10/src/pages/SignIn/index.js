527369e9a4e15a39378ca3df37b4d705a583a92c
Luiz Ortega <luizngust@hotmail.com>
2019-08-13 00:22:35 -0300

    SignIn

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '../../components/Input';
+import logo from '../../assets/logo.png';
+
 import Button from '../../components/Button';
 import Background from '../../components/Background';
 
-// import { Container } from './styles';
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
 export default function SignIn() {
     return (
         <Background>
-            <Text>SignIn</Text>
+            <Container>
+                <Image source={logo} />
 
-            <Input
-                style={{ marginTop: 30 }}
-                icon="call"
-                placeholder="Digite seu telefone"
-            />
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalized="none"
+                        placeholder="Digite seu e-mail"
+                    />
 
-            <Button>Entrar</Button>
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua senha secreta"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => {}}>
+                    <SignLinkText>Criar conta gratuita</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 }
