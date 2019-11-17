1fd440e2d81a90203d7bd8a899f304fe202469b6
luiz-ortega <luizngust@hotmail.com>
2019-11-17 20:23:44 -0300

    feat: Page de SignIn.

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Image} from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
 export default function SignIn() {
     return (
         <Background>
-            <SafeAreaView>
-                <Text>SignIn</Text>
+            <Container>
+                <Image source={logo} />
 
-                <Input
-                    style={{marginTop: 30}}
-                    icon="call"
-                    placeholder="Digite seu nome..."
-                />
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
 
-                <Button>Entrar</Button>
-            </SafeAreaView>
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry={false}
+                        placeholder="Sua senha secreta"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+
+                    <SignLink onPress={() => {}}>
+                        <SignLinkText>Criar conta gratuita</SignLinkText>
+                    </SignLink>
+                </Form>
+            </Container>
         </Background>
     );
 }
