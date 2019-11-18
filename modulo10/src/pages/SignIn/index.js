cdaabb89d3568c9ebf6450c1883dfd899a19a4ba
luiz-ortega <luizngust@hotmail.com>
2019-11-17 21:05:45 -0300

    feat: Acessibilidade. Navegação entre inputs por refs e configuração de inputs.

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import PropTypes from 'prop-types';
 import {Image} from 'react-native';
 
 import logo from '~/assets/logo.png';
@@ -15,6 +16,10 @@ import {
 } from './styles';
 
 export default function SignIn({navigation}) {
+    const passwordRef = useRef();
+
+    function handleSubmit() {}
+
     return (
         <Background>
             <Container>
@@ -27,12 +32,17 @@ export default function SignIn({navigation}) {
                         autoCorrect
                         autoCapitalize="none"
                         placeholder="Digite seu e-mail"
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
                     />
 
                     <FormInput
                         icon="lock-outline"
-                        secureTextEntry={false}
+                        secureTextEntry
                         placeholder="Sua senha secreta"
+                        ref={passwordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
                     />
 
                     <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
@@ -45,3 +55,7 @@ export default function SignIn({navigation}) {
         </Background>
     );
 }
+
+SignIn.propTypes = {
+    navigation: PropTypes.objectOf.isRequired,
+};
--- modulo10/src/pages/SignUp/index.js
+++ modulo10/src/pages/SignUp/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import PropTypes from 'prop-types';
 import {Image} from 'react-native';
 
 import logo from '~/assets/logo.png';
@@ -15,6 +16,11 @@ import {
 } from './styles';
 
 export default function SignUp({navigation}) {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    function handleSubmit() {}
+
     return (
         <Background>
             <Container>
@@ -26,6 +32,8 @@ export default function SignUp({navigation}) {
                         autoCorrect={false}
                         autoCapitalize="none"
                         placeholder="Nome completo"
+                        returnKeyType="next"
+                        onSubmitEditing={() => emailRef.current.focus()}
                     />
                     <FormInput
                         icon="mail-outline"
@@ -33,21 +41,31 @@ export default function SignUp({navigation}) {
                         autoCorrect
                         autoCapitalize="none"
                         placeholder="Digite seu e-mail"
+                        ref={emailRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
                     />
 
                     <FormInput
                         icon="lock-outline"
                         secureTextEntry={false}
                         placeholder="Sua senha secreta"
+                        ref={passwordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
                     />
 
-                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
 
-                    <SignLink onPress={() => navigation.navigate('SignUp')}>
-                        <SignLinkText>Criar conta gratuita</SignLinkText>
+                    <SignLink onPress={() => navigation.navigate('SignIn')}>
+                        <SignLinkText>Já tenho conta</SignLinkText>
                     </SignLink>
                 </Form>
             </Container>
         </Background>
     );
 }
+
+SignUp.propTypes = {
+    navigation: PropTypes.objectOf.isRequired,
+};
