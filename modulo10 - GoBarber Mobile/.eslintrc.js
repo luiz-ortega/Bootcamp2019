b4eda1bf3eaa270661e4ea2ba4e0387e48e22ead
luiz-ortega <luizngust@hotmail.com>
2019-11-21 01:39:30 -0300

    feat: Conectando redux.

--- modulo10 - GoBarber Mobile/.eslintrc.js
+++ modulo10 - GoBarber Mobile/.eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
         'no-console': ['error', {allow: ['tron']}],
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
+        'react/prop-types': ['error', {ignore: ['navigation']}],
     },
     settings: {
         'import/resolver': {
--- modulo10 - GoBarber Mobile/src/pages/SignIn/index.js
+++ modulo10 - GoBarber Mobile/src/pages/SignIn/index.js
@@ -1,10 +1,12 @@
-import React, {useRef} from 'react';
-import PropTypes from 'prop-types';
+import React, {useRef, useState} from 'react';
 import {Image} from 'react-native';
+import PropTypes from 'prop-types';
+import {useDispatch, useSelector} from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import {signInRequest} from '~/store/modules/auth/actions';
 
 import {
     Container,
@@ -15,11 +17,18 @@ import {
     SignLinkText,
 } from './styles';
 
-export default function SignUp({navigation}) {
-    const emailRef = useRef();
+export default function SignIn({navigation}) {
+    const dispatch = useDispatch();
     const passwordRef = useRef();
 
-    function handleSubmit() {}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loading = useSelector(state => state.auth.loading);
+
+    function handleSubmit() {
+        dispatch(signInRequest(email, password));
+    }
 
     return (
         <Background>
@@ -28,37 +37,34 @@ export default function SignUp({navigation}) {
 
                 <Form>
                     <FormInput
-                        icon="person-outline"
-                        autoCorrect={false}
-                        autoCapitalize="none"
-                        placeholder="Nome completo"
-                        returnKeyType="next"
-                        onSubmitEditing={() => emailRef.current.focus()}
-                    />
-                    <FormInput
                         icon="mail-outline"
                         keyboardType="email-address"
                         autoCorrect
                         autoCapitalize="none"
                         placeholder="Digite seu e-mail"
-                        ref={emailRef}
                         returnKeyType="next"
                         onSubmitEditing={() => passwordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
                     />
 
                     <FormInput
                         icon="lock-outline"
-                        secureTextEntry={false}
+                        secureTextEntry
                         placeholder="Sua senha secreta"
                         ref={passwordRef}
                         returnKeyType="send"
                         onSubmitEditing={handleSubmit}
+                        value={password}
+                        onChangeText={setPassword}
                     />
 
-                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+                    <SubmitButton loading={loading} onPress={handleSubmit}>
+                        Acessar
+                    </SubmitButton>
 
-                    <SignLink onPress={() => navigation.navigate('SignIn')}>
-                        <SignLinkText>Já tenho conta</SignLinkText>
+                    <SignLink onPress={() => navigation.navigate('SignUp')}>
+                        <SignLinkText>Criar conta gratuita</SignLinkText>
                     </SignLink>
                 </Form>
             </Container>
@@ -66,6 +72,8 @@ export default function SignUp({navigation}) {
     );
 }
 
-SignUp.propTypes = {
-    navigation: PropTypes.func.isRequired,
+SignIn.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+    }).isRequired,
 };
