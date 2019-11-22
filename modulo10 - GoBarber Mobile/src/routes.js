d7d2507a5620009079941b2701aad5238b79691c
luiz-ortega <luizngust@hotmail.com>
2019-11-22 00:07:50 -0300

    feat: Redirecionamento para dashboard e configuração das rotas de acordo com status signed do usuário.

--- modulo10 - GoBarber Mobile/src/routes.js
+++ modulo10 - GoBarber Mobile/src/routes.js
@@ -1,11 +1,25 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-export default createAppContainer(
-    createSwitchNavigator({
-        SignIn,
-        SignUp,
-    })
-);
+import DashBoard from './pages/Dashboard';
+
+export default (signedIn = false) =>
+    createAppContainer(
+        createSwitchNavigator(
+            {
+                Sign: createSwitchNavigator({
+                    SignIn,
+                    SignUp,
+                }),
+                App: createBottomTabNavigator({
+                    DashBoard,
+                }),
+            },
+            {
+                initialRouteName: signedIn ? 'App' : 'Sign',
+            }
+        )
+    );
