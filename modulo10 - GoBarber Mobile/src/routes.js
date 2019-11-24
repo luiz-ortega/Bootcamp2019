3ef7e17216d4f8856a5493566a95dac4e3da0c91
luiz-ortega <luizngust@hotmail.com>
2019-11-24 14:29:43 -0300

    feat: estilização das rotas.

--- modulo10 - GoBarber Mobile/src/routes.js
+++ modulo10 - GoBarber Mobile/src/routes.js
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import DashBoard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (signedIn = false) =>
     createAppContainer(
@@ -14,9 +15,22 @@ export default (signedIn = false) =>
                     SignIn,
                     SignUp,
                 }),
-                App: createBottomTabNavigator({
-                    DashBoard,
-                }),
+                App: createBottomTabNavigator(
+                    {
+                        DashBoard,
+                        Profile,
+                    },
+                    {
+                        tabBarOptions: {
+                            keyboardHidesTabBar: true,
+                            activeTintColor: '#fff',
+                            inactiveTintColor: 'rgba(255,255,255,0.6)',
+                            style: {
+                                backgroundColor: '#8d41a8',
+                            },
+                        },
+                    }
+                ),
             },
             {
                 initialRouteName: signedIn ? 'App' : 'Sign',
