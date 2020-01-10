c48cbfe2e50150a69df0269415f067c9b8550630
luiz-ortega <luizngust@hotmail.com>
2020-01-10 00:36:41 -0300

    feat: Confirmando agendamento.

--- modulo10 - GoBarber Mobile/src/services/api.js
+++ modulo10 - GoBarber Mobile/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://localhost:3334',
-    baseURL: 'http://10.0.2.2:3334',
+    baseURL: 'http://localhost:3334',
+    // baseURL: 'http://10.0.2.2:3334',
 });
 
 export default api;
