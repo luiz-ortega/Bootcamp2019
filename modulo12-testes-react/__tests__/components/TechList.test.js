afc0007714c265748cf2bc8325863a923123cd91
luiz-ortega <luizngust@hotmail.com>
2020-04-28 19:41:03 -0300

    Mock redux - useSelector

--- modulo12-testes-react/__tests__/components/TechList.test.js
+++ modulo12-testes-react/__tests__/components/TechList.test.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 import { render, fireEvent, cleanup } from '@testing-library/react';
 
 import TechList from '~/components/TechList';
 
+jest.mock('react-redux');
+
 describe('TechList component', () => {
-    beforeEach(() => {
-        localStorage.clear();
-    })
+    // beforeEach(() => {
+    //     localStorage.clear();
+    // })
 
-    it('should be able to add new tech', () => {
-        const { getByText, getByTestId, debug, getByLabelText} = render( <TechList /> );
+    // it('should be able to add new tech', () => {
+    //     const { getByText, getByTestId, debug, getByLabelText} = render( <TechList /> );
 
-        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
-        fireEvent.submit(getByTestId('tech-form'));        
+    //     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    //     fireEvent.submit(getByTestId('tech-form'));        
 
-        expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-        expect(getByLabelText('Tech')).toHaveValue('');
-    });
+    //     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    //     expect(getByLabelText('Tech')).toHaveValue('');
+    // });
 
-    it('should store techs in storage', () => {
-        let { getByText, getByTestId, debug, getByLabelText, unmount} = render( <TechList /> );
+    // it('should store techs in storage', () => {
+    //     let { getByText, getByTestId, debug, getByLabelText, unmount} = render( <TechList /> );
     
-        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
-        fireEvent.submit(getByTestId('tech-form'));
+    //     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    //     fireEvent.submit(getByTestId('tech-form'));
 
-        unmount();
+    //     unmount();
      
-        ({ getByText, getByTestId, unmount } = render( <TechList /> ));
+    //     ({ getByText, getByTestId, unmount } = render( <TechList /> ));
 
-        expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
+    //     expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
+    //     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    // })
+
+    it('should render tech list', () => {   
+        useSelector.mockImplementation(cb => cb({
+            techs: ['Node.js', 'ReactJS']
+        }));
+
+        const { getByTestId, getByText } = render(<TechList />);
+
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+        expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
     })
 })
--- modulo12-testes-react/src/components/TechList/index.js
+++ modulo12-testes-react/src/components/TechList/index.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function TechList() {
-  const [ techs, setTechs] = useState([]);
+  // const [ techs, setTechs] = useState([]);
   const [ newTech, setNewTech ] = useState('');
 
-  useEffect(() => {
-    const techs = localStorage.getItem('techs');
+  const dispatch = useDispatch();
+  const techs = useSelector(state => state.techs);
 
-    if (techs) {
-      setTechs(JSON.parse(techs));
-    }
-  }, [])
+  // useEffect(() => {
+  //   const techs = localStorage.getItem('techs');
 
-  useEffect(() => {
-    localStorage.setItem('techs', JSON.stringify(techs));
-  }, [techs])
+  //   if (techs) {
+  //     setTechs(JSON.parse(techs));
+  //   }
+  // }, [])
+
+  // useEffect(() => {
+  //   localStorage.setItem('techs', JSON.stringify(techs));
+  // }, [techs])
 
   function handleAddTech() {
-    setTechs([...techs, newTech]);
+    // setTechs([...techs, newTech]);
+    dispatch({ type: 'ADD_TECH', payload: { tech: newTech } })
     setNewTech('');
   }
   
